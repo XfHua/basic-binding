9c1356905dd4115229a2c1649132668c1468219a
XfHua <jasonHuaxf@163.com>
2020-07-31 15:30:58 +0800

    Add files via upload

---
+++ App332/App332/App332.Android/MyCollectionViewRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+
+using Xamarin.Forms;
+using App332.Droid;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.CollectionView), typeof(MyCollectionViewRenderer))]
+
+namespace App332.Droid
+{
+    class MyCollectionViewRenderer : CollectionViewRenderer
+    {
+        public MyCollectionViewRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<ItemsView> elementChangedEvent)
+        {
+            base.OnElementChanged(elementChangedEvent);
+
+            this.ScrollbarFadingEnabled = false;
+        }
+    }
+}
---
+++ App332/App332/App332/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App332
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        FBProduct testP { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            testP  = new FBProduct();
+
+            testP.ProductName = "Apple";
+            testP.ProductPrice = "37.95";
+            testP.ProductImage = "Image goes here";
+            testP.ShoppingCartItemTotals = "default vaule of ShoppingCartItemTotals";
+
+            BindingContext = testP;
+
+        }
+
+        private void stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            //get qty
+            double qty = e.NewValue;
+
+            Stepper currentStepper = sender as Stepper;
+
+            FBProduct currentProduct = currentStepper.BindingContext as FBProduct;
+
+            //get price
+            double price = Convert.ToDouble(currentProduct.ProductPrice);
+
+            testP.ShoppingCartItemTotals = (qty * price).ToString();
+
+        }
+    }
+
+    public class FBProduct : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string ProductName { get; internal set; }
+        public string ProductPrice { get; internal set; }
+        public string ProductImage { get; internal set; }
+
+        string shoppingCartItemTotals;
+
+        public string ShoppingCartItemTotals
+        {
+            set
+            {
+                if (shoppingCartItemTotals != value)
+                {
+                    shoppingCartItemTotals = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("ShoppingCartItemTotals"));
+                    }
+                }
+            }
+            get
+            {
+                return shoppingCartItemTotals;
+            }
+        }
+    }
+
+}
